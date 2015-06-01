7b7af84619db6849e4aada8cd5d453c6523dca3a
iskander <quasilyte@gmail.com>
2015-06-01 22:15:25 +0300

    fixed a mistake + 1 new task completed

--- 1_16.rkt
+++ 1_16.rkt
@@ -1,20 +1,24 @@
 #lang racket
 
-(letrec ([fast-exp (λ (base pow)
-  (cond ([= 0 pow] 1)
-    ([even? pow] (sqr (fast-exp base (/ pow 2))))
-    (else (* base (fast-exp base (- pow 1))))))])
-  (displayln (fast-exp 6 6)))
+(provide fast-exp)
+
+; (letrec ([fast-exp (λ (base pow)
+;   (cond ([= 0 pow] 1)
+;     ([even? pow] (sqr (fast-exp base (/ pow 2))))
+;     (else (* base (fast-exp base (- pow 1))))))])
+;   (displayln (fast-exp 6 6)))
 
 (define (fast-exp base pow)
   (define (reduction-possible? pow)
     (and (even? pow) (> pow 2)))
   (define (fast-exp-iter base pow acc)
-    (define-syntax-rule (base^2)
-      (* base base))
     (cond ([= 0 pow] acc)
-      ([reduction-possible? pow] (fast-exp-iter (base^2) (/ pow 2) 1))
+      ([reduction-possible? pow] (fast-exp-iter (sqr base) (/ pow 2) 1))
       (else (fast-exp-iter base (- pow 1) (* acc base)))))
-  (fast-exp-iter base pow 1))
+  (if (reduction-possible? (sub1 pow))
+    (* base (fast-exp-iter (sqr base) (/ (sub1 pow) 2) 1))
+    (fast-exp-iter base pow 1)))
 
-(displayln (fast-exp 4 32)) ; => 18446744073709551616, 7 steps
+; (displayln (fast-exp 4 32)) ; => 18446744073709551616, 7 steps
+; (displayln (fast-exp 4 5)) ; => 1024, 3 steps
+
---
+++ 1_17.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(provide fast-mul)
+
+(define (mul-guard fn a b)
+  (cond ([= 0 b] 0)
+    ([> b 0] (fn a b))
+    (else (- (fn a (- b))))))
+
+(define (mul a b)
+  (define (mul-iter acc b)
+    (if (= 1 b) acc
+      (mul-iter (+ acc a) (sub1 b))))
+  (mul-guard mul-iter a b))
+
+(define (fast-mul a b)
+  (define (reduction-possible? b)
+    (and (even? b) (> b 2)))
+  (define (reduce a b)
+    (list (* a 2) (/ b 2) (* a 2)))
+  (define (fast-mul-iter a b acc)
+    (cond ([= 1 b] acc)
+      ([reduction-possible? b] (apply fast-mul-iter (reduce a b)))
+      (else (fast-mul-iter a (sub1 b) (+ acc a)))))
+  (define (entry-point a b)
+    (if (reduction-possible? (- b 1))
+      (+ a (apply fast-mul-iter (reduce a (- b 1))))
+      (fast-mul-iter a b a)))
+  (mul-guard entry-point a b))
+
+; (for ([i 6])
+;   (let ([result (mul 5 i)] [fast-result (fast-mul 5 i)])
+;     (display (list 5 "*" i "=" fast-result))
+;     (displayln (= (* 5 i) result fast-result))))
